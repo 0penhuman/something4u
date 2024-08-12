128521f37fd832476bb08246eccc1e8bcafcef16
warm <159608807+0penhuman@users.noreply.github.com>
2024-08-12 22:01:13 +0800

    Update snake.cpp

--- snake.cpp
+++ snake.cpp
@@ -6,18 +6,20 @@ bool isdead=false;
 struct snake
 {
     int8_t position;//position=x<<3+y
-    /*
-    解析direction：
-        奇数为左右，偶数为上下
-        up为2的奇数倍，down为2的偶数倍
-        left>0:     (left+1)/2%2=1
-        left<0:     (-left+1)/2%2=0
-        right>0:    (right+1)/2%2=0
-        right<0:    (-right+1)/2%2=1
-    */
-   int8_t direction;//left:+1  right:-1,3    down:0    up:+2,-2
+   int8_t direction;
     snake* next;
 };
+int8_t decodeDirection(int8_t direction){//解析direction    left:+1  right:-1    down:0    up:+2
+    if(direction%2==0){
+        if(direction/2%2==0)return 0;
+        else return 2;
+    }
+    else{
+        if((direction+1)/2%2==0)return -1;
+        else return 1;
+    }
+}
+
 snake* head=new snake;
 void init_field(){//初始化活动区域
     for(int8_t row=0;row<8;row++){
@@ -27,45 +29,84 @@ void init_field(){//初始化活动区域
     }
 }
 void init_snake(){
-    head->position=(4<<3);//这表明它位于(4,4)
+    head->position=(4<<3)+3;//这表明它位于(4,4)
     head->direction=0;
 }
-void godown(snake* temp){
-    if(temp->next==NULL){//如果出现最后一个点没动，则修改这里//也许需要分类讨论
-        if(head->position-(head->position>>3<<3)==7)isdead=true;
-        else head->position+=1;//head->direction+0;
-        return;
-    };
-    godown(temp->next);
+
+void goforward(snake* temp){
+    if(temp==head){
+        switch (decodeDirection(head->direction))
+        {
+        case 1://左
+            if(head->position>>3==0)isdead=1;
+            else head->position-=1<<3;
+            break;
+        case -1://右
+            if(head->position>>3==7)isdead=1;
+            else head->position+=1<<3;
+            break;
+        case 0://下
+            if(head->position-(head->position>>3<<3)==7)isdead=1;
+            else head->position+=1;
+            break;
+        case 2://上
+            if(head->position-(head->position>>3<<3)==0)isdead=1;
+            else head->position-=1;
+            break;
+        default:
+            break;
+        }
+    }
+    if(temp->next==NULL)return;
+    goforward(temp->next);
     temp->next->position=temp->position;
     temp->next->direction=temp->direction;
 }
 
-void goforward(snake* temp){
-    if(temp->next=NULL){
-        if(head->position>>3==0&&abs(head->direction/2%2)==1)
-        /*
-        现在是8.12 我正在写goforward函数，我打算以godown为基础，这些if是用于判断死亡条件的
-        */
-    }
+void turnLeft(){
+    head->direction-=1;
+}
+void turnRight(){
+    head->direction+=1;
 }
 
 void grow(){//body长度只与t有关
     snake* temp=head;
     for(;temp->next!=NULL;temp=temp->next){}
-    if(temp->direction%2==0){
-        if(temp->direction/2%2==0){//表明上一个节点的运动方向向下
-            if(temp->position-(temp->position>>3<<3)==7)isdead=true;
+    switch (decodeDirection(temp->direction))
+    {
+    case -1://右
+        if(temp->position>>3==0)isdead=1;
+        else{
+        snake* body=new snake;
+        body->position=temp->position+(1<<3);
+        body->direction=temp->direction;
+        if(temp!=head)temp->next=body;
+        else head->next=body;
+        body->next=NULL;
+        }
+        break;
+    case 1://左
+    if(temp->position>>3==0)isdead=1;
+            else{
+            snake* body=new snake;
+            body->position=temp->position-(1<<3);
+            body->direction=temp->direction;
+            if(temp!=head)temp->next=body;
+            else head->next=body;
+            body->next=NULL;}
+        break;
+    case 0://下
+    if(temp->position-(temp->position>>3<<3)==7)isdead=1;
             else{snake* body=new snake;
             body->position=temp->position-1;
             body->direction=temp->direction;
             if(temp!=head)temp->next=body;
             else head->next=body;
             body->next=NULL;}
-        }
-        
-        else{//表明上一个节点的运动方向向上
-            if(temp->position-(temp->position>>3<<3)==0)isdead=true;
+        break;
+    case 2://上
+        if(temp->position-(temp->position>>3<<3)==0)isdead=1;
             else{
             snake* body=new snake;
             body->position=temp->position+1;
@@ -74,42 +115,18 @@ void grow(){//body长度只与t有关
             else head->next=body;
             body->next=NULL;
             }
-        }
-    }
-
-    else
-    {
-        if((temp->direction+1)/2%2==0){//表明上一个节点的运动方向向右
-            if(temp->position>>3==0)isdead=true;
-            else{
-            snake* body=new snake;
-            body->position=temp->position+(1<<3);
-            body->direction=temp->direction;
-            if(temp!=head)temp->next=body;
-            else head->next=body;
-            body->next=NULL;
-            }
-        }
-        else{
-            if(temp->position>>3==0)isdead=true;
-            else{
-            snake* body=new snake;
-            body->position=temp->position-(1<<3);
-            body->direction=temp->direction;
-            if(temp!=head)temp->next=body;
-            else head->next=body;
-            body->next=NULL;
-            }
-        }
+        break;
     }
 }
 
-
-
-
-
-
-
+void iseatbody(snake* temp){
+    if(temp==NULL)return;
+    if(temp!=head&&temp!=head->next&&temp->position==head->position){
+        isdead=1;
+        return;
+    }
+    iseatbody(temp->next);
+}
 
 void display(){
     snake* temp=head;
@@ -118,8 +135,8 @@ void display(){
         temp=temp->next;
     }
     for(int8_t row=0;row<8;row++){
-        for(int8_t conlumn=0;conlumn<8;conlumn++){
-            cout<<field[conlumn][row]<<'\t';
+        for(int8_t column=0;column<8;column++){
+            cout<<field[column][row]<<'\t';
         }
         cout<<'\n';
     }
@@ -127,12 +144,19 @@ void display(){
 int main(){
     init_field();
     init_snake();
+    char ch;
+    grow();
+    
     while(!isdead){
         display();
-        godown(head);
+        iseatbody(head);
+        goforward(head);
         grow();
-        cin.get();
+        cin.get(ch);
+        if(ch=='r')turnRight();
+        else if(ch=='l')turnLeft();
         system("clear");
         init_field();
+        head->direction%=4;
     }
 }
